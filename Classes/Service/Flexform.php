9b8c832f2110fda884ed65940803647dd6b70176
ssp <porst@sub.uni-goettingen.de>
2011-09-19 09:53:03 +0200

    MIT License

--- Classes/Service/Flexform.php
+++ Classes/Service/Flexform.php
@@ -1,16 +1,29 @@
 <?php
-/*************************************************************************
- *  Copyright notice
+/*******************************************************************************
+ * Copyright notice
  *
- *  © 2011 Sven-S. Porst, SUB Göttingen <porst@sub.uni-goettingen.de>
- *  All rights reserved
+ * Copyright (C) 2011 by Sven-S. Porst, SUB Göttingen
+ * <porst@sub.uni-goettingen.de>
  *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  This copyright notice MUST APPEAR in all copies of the script.
- *************************************************************************/
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
 
 class tx_Pazpar2_Service_Flexform {
 
